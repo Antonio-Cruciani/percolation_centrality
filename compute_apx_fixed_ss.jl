4d38e6fc18ec1da3e506a5621514fec237ed815a
Antonio <antonio.cruciani@gssi.it>
2024-05-31 12:48:45 +0200

    new experiments

--- compute_apx_fixed_ss.jl
+++ compute_apx_fixed_ss.jl
@@ -2,14 +2,14 @@ include("src/PERC.jl")
 
 
 #Common parameters
-epsilon_list = [0.07,0.05,0.01,0.005]
-ss_save = [2,3,4,5]
+epsilon_list = [0.1,0.07,0.05,0.01,0.005,0.001]
+ss_save = [1,2,3,4,5,6]
 delta = 0.1
 run = 10
 
 
 # Undirected
-#=
+
 datasets = ["00_hiv.txt","00_ego-fb-combined-N.txt","01_musae_facebook_edges.txt","02_email_enron.txt","03_ca_astroph.txt","07_large_twitch_edges.txt"]
 directed = false
 separator = "\t"
@@ -22,16 +22,18 @@ for ds in datasets
     i = 1
     for epsilon in epsilon_list
         for _ in 1:run
+            @info("Computing Apx percolation centrality for "*ds_name)
+            flush(stderr)
             x = parallel_estimate_percolation_centrality_fixed_sample_size(g,perc,epsilon,delta)
             save_results_progressive_sampling(ds_name,"fixed_ss",x[1],x[2],x[3],ss_save[i],epsilon)
         end
         i+=1
     end
 end
-=#
+
 # Directed
 #datasets = ["11_soc_epinions.txt","12_soc_slashdot.txt","04_web_notredame.txt","06_web_google.txt","08_web_berkstan.txt"]
-datasets =  ["15_cit_hepph.txt","14_p2p_gnutella31.txt"]
+datasets = ["15_cit_hepph.txt","14_p2p_gnutella31.txt","11_soc_epinions.txt","12_soc_slashdot.txt","04_web_notredame.txt","06_web_google.txt"]
 
 directed = true
 separator = "\t"
@@ -44,59 +46,11 @@ for ds in datasets
     i = 1
     for epsilon in epsilon_list
         for _ in 1:run
+            @info("Computing Apx percolation centrality for "*ds_name)
+            flush(stderr)
             x = parallel_estimate_percolation_centrality_fixed_sample_size(g,perc,epsilon,delta)
             save_results_progressive_sampling(ds_name,"fixed_ss",x[1],x[2],x[3],ss_save[i],epsilon)
         end
         i+=1
     end
 end
-
-#=
-epsilon_list = [0.001,0.0005]
-ss_save = [6,7]
-
-run = 1
-
-
-
-# Undirected
-datasets = ["00_hiv.txt","00_ego-fb-combined-N.txt","01_musae_facebook_edges.txt","02_email_enron.txt","03_ca_astroph.txt","07_large_twitch_edges.txt"]
-directed = false
-separator = "\t"
-
-for ds in datasets
-    gf = "graphs/"*ds
-    g = load_graph(gf,directed,separator)
-    perc = read_percolation("percolation_states/"*ds)
-    ds_name = string(split(ds,".txt")[1])
-    i = 1
-    for epsilon in epsilon_list
-        for _ in 1:run
-            x = parallel_estimate_percolation_centrality_fixed_sample_size(g,perc,epsilon,delta)
-            save_results_progressive_sampling(ds_name,"fixed_ss",x[1],x[2],x[3],ss_save[i],epsilon)
-        end
-        i+=1
-    end
-end
-
-# Directed
-datasets = ["11_soc_epinions.txt","12_soc_slashdot.txt","04_web_notredame.txt","06_web_google.txt","08_web_berkstan.txt"]
-directed = true
-separator = "\t"
-
-for ds in datasets
-    gf = "graphs/"*ds
-    g = load_graph(gf,directed,separator)
-    perc = read_percolation("percolation_states/"*ds)
-    ds_name = string(split(ds,".txt")[1])
-    i = 1
-    for epsilon in epsilon_list
-        for _ in 1:run
-            x = parallel_estimate_percolation_centrality_fixed_sample_size(g,perc,epsilon,delta)
-            save_results_progressive_sampling(ds_name,"fixed_ss",x[1],x[2],x[3],ss_save[i],epsilon)
-        end
-        i+=1
-    end
-end
-
-=#
