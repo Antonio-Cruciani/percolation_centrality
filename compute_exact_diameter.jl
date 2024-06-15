2c11cca4035ff7798ce0ee14270fa46a8b26d490
Antonio <antonio.cruciani@gssi.it>
2024-06-15 18:30:26 +0200

    bugfix

--- compute_exact_diameter.jl
+++ compute_exact_diameter.jl
@@ -15,7 +15,6 @@ for ds in datasets
     @info("Computing Ground Truth distance based metrics for "*ds_name)
     flush(stderr)
     x = parallel_random_bfs_rho(g,0)
-    println(x)
     save_diameter(ds_name,x[1],x[2],x[3])
 end
 
@@ -23,7 +22,6 @@ end
 datasets = ["15_cit_hepph.txt","14_p2p_gnutella31.txt","11_soc_epinions.txt","12_soc_slashdot.txt","04_web_notredame.txt","06_web_google.txt","08_web_berkstan.txt"]
 #datasets = ["06_web_google.txt","08_web_berkstan.txt","09_italian_twitter.txt","13_soc_pokec.txt","05_wiki_talk.txt"]
 
-perc = random_percolations(nv(g))
 
 directed = true
 
--- src/utilities/stats.jl
+++ src/utilities/stats.jl
@@ -1,3 +1,8 @@
+
+#using Conda
+using PyCall
+#Conda.add("scipy")
+
 function supremum_deviation(x::Array{Float64},y::Array{Float64})::Float64
     @assert lastindex(x) == lastindex(y) "x and y must have the same length"
     sd::Float64 = 0.0
@@ -8,3 +13,55 @@ function supremum_deviation(x::Array{Float64},y::Array{Float64})::Float64
     end
     return sd
 end
+
+
+
+
+py"""
+from scipy import stats
+def ktau(x, y):
+    return stats.kendalltau(x, y)
+def wktau(x, y):
+    return stats.weightedtau(x, y)
+def spear(x,y):
+    return stats.spearmanr(x, y)
+def pear(x,y):
+    return stats.pearsonr(x, y)
+"""
+
+function compute_correlations(x, y, verbose::Bool)
+    sp = py"spear"(x, y)
+    if (verbose)
+        log("    Spearman computed")
+    end
+    kt = py"ktau"(x, y)
+    if (verbose)
+        log("    Kendall tau computed")
+    end
+    wkt = py"wktau"(x, y)
+    if (verbose)
+        log("    Weighted Kendall tau computed")
+    end
+    p =  py"pear"(x, y)
+    if (verbose)
+        log("    Pearson computed")
+    end
+    return sp, kt, wkt,p
+end
+
+function normalize_centrality(x::Array{Float64})::Array{Float64}
+    n::Int64 = length(x)
+    for i in 1:n
+        x[i] = x[i]/(n*(n-1))
+    end
+    return x
+end
+
+
+
+
+
+
+
+
+
