1ce1f08dea6bc0a2dd571ae4ee1b245dcf759c3f
Antonio <antonio.cruciani@gssi.it>
2024-10-24 15:13:32 +0530

    new experiments fixed ss

--- compute_apx_fixed_ss.jl
+++ compute_apx_fixed_ss.jl
@@ -8,7 +8,9 @@ ss_save = [1,2,3,4,5]
 run = 10
 
 
-datasets = ["00_hiv.txt","00_ego-fb-combined-N.txt","01_musae_facebook_edges.txt","02_email_enron.txt","03_ca_astroph.txt","07_large_twitch_edges.txt","10_flickr.txt"]
+#datasets = ["00_hiv.txt","00_ego-fb-combined-N.txt","01_musae_facebook_edges.txt","02_email_enron.txt","03_ca_astroph.txt","07_large_twitch_edges.txt","10_flickr.txt"]
+datasets = ["com-youtube.ungraph.txt","com-lj.ungraph.txt","com-orkut.ungraph.txt"]
+
 # Undirected
 directed = false
 separator = "\t"
@@ -31,7 +33,7 @@ end
 
 
 
-
+#=
 # Directed
 datasets = ["15_cit_hepph.txt","14_p2p_gnutella31.txt","11_soc_epinions.txt","12_soc_slashdot.txt","04_web_notredame.txt","06_web_google.txt","08_web_berkstan.txt","05_wiki_talk.txt","09_italian_twitter.txt","13_soc_pokec.txt"]
 
@@ -55,3 +57,4 @@ for ds in datasets
         i+=1
     end
 end
+=#
