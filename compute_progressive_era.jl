c83dbead2bbcc008e1fc2f3b1b6c6ec30abf4342
Antonio <antonio.cruciani@gssi.it>
2024-11-14 12:27:02 +0100

    new experiment era

--- compute_progressive_era.jl
+++ compute_progressive_era.jl
@@ -9,18 +9,19 @@ delta = 0.1
 run = 5
 vc_ub = false
 
-
+#=
 graphs_path = "/emmynas/pasquini/data_percolation/graphs/"
 percolation_path = "/emmynas/pasquini/data_percolation/percolation_states/"
 output_path = "/emmynas/pasquini/data_percolation/"
-#=
+=#
 graphs_path = "graphs/"
 percolation_path = "percolation_states/"
 output_path = ""
-=#
+
 # Undirected
-datasets = ["01_musae_facebook_edges.txt","02_email_enron.txt","03_ca_astroph.txt","07_large_twitch_edges.txt","10_flickr.txt","com-youtube.ungraph.txt","com-lj.ungraph.txt","com-orkut.ungraph.txt"]
-#datasets = ["com-youtube.ungraph.txt","com-lj.ungraph.txt","com-orkut.ungraph.txt"]
+#datasets = ["01_musae_facebook_edges.txt","02_email_enron.txt","03_ca_astroph.txt","07_large_twitch_edges.txt","10_flickr.txt","com-youtube.ungraph.txt","com-lj.ungraph.txt","com-orkut.ungraph.txt"]
+datasets = ["com-youtube.ungraph.txt","com-lj.ungraph.txt","com-orkut.ungraph.txt"]
+
 
 
 
@@ -43,7 +44,7 @@ for ds in datasets
     end
 end
 
-
+#=
 # Directed
 datasets = ["15_cit_hepph.txt","14_p2p_gnutella31.txt","11_soc_epinions.txt","12_soc_slashdot.txt","04_web_notredame.txt","06_web_google.txt","08_web_berkstan.txt","05_wiki_talk.txt","09_italian_twitter.txt","13_soc_pokec.txt"]
 
@@ -67,3 +68,4 @@ for ds in datasets
     end
 end
 
+=#
