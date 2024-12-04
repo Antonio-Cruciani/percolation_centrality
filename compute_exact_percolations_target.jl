378cb0f4ff5f4c465debdbc99af931e5f21133c6
Antonio <antonio.cruciani@gssi.it>
2024-12-04 18:03:12 +0100

    added runner for second function

---
+++ compute_exact_percolations_target.jl
@@ -0,0 +1,42 @@
+include("src/PERC.jl")
+
+# Undirected
+
+datasets = ["00_hiv.txt","00_ego-fb-combined-N.txt","01_musae_facebook_edges.txt","02_email_enron.txt"]
+directed = false
+
+separator = "\t"
+normalized = false
+graphs_path = "graphs/"
+for ds in datasets
+    gf = graphs_path*ds
+    g = load_graph(gf,directed,separator)
+    perc = read_percolation(percolation_path*ds)
+    ds_name = string(split(ds,".txt")[1])
+    @info("Computing Ground Truth percolation centrality for "*ds_name)
+    flush(stderr)
+    
+    x = parallel_percolation_centrality_target(g,perc,normalized)
+    save_results(ds_name,"exact_target",x[1],x[2])
+end
+
+# Directed
+datasets = ["15_cit_hepph.txt","14_p2p_gnutella31.txt","11_soc_epinions.txt","12_soc_slashdot.txt"]
+
+
+directed = true
+
+separator = "\t"
+normalized = false
+for ds in datasets
+    gf = graphs_path*ds
+    g = load_graph(gf,directed,separator)
+    perc = read_percolation(percolation_path*ds)
+    ds_name = string(split(ds,".txt")[1])
+    @info("Computing Ground Truth percolation centrality for "*ds_name)
+    flush(stderr)
+    x = parallel_percolation_centrality_target(g,perc,normalized)
+    save_results(ds_name,"exact_target",x[1],x[2])
+end
+
+
