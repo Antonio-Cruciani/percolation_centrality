763af20f845ad7987e3e786a70321a6d83d386b9
Antonio <antonio.cruciani@gssi.it>
2024-12-04 17:27:08 +0100

    new states

--- generate_random_percolation_states.jl
+++ generate_random_percolation_states.jl
@@ -2,14 +2,14 @@ include("src/PERC.jl")
 
 
 
-datasets = ["com-youtube.ungraph.txt","com-lj.ungraph.txt","com-orkut.ungraph.txt"]
+datasets = ["00_hiv.txt","00_ego-fb-combined-N.txt","01_musae_facebook_edges.txt","02_email_enron.txt","03_ca_astroph.txt","07_large_twitch_edges.txt","15_cit_hepph.txt","14_p2p_gnutella31.txt","11_soc_epinions.txt","12_soc_slashdot.txt","04_web_notredame.txt","06_web_google.txt","08_web_berkstan.txt"]
 
 
 
 directed = false
 
 separator = "\t"
-normalized = false
+
 for ds in datasets
     gf = "graphs/"*ds
     g = load_graph(gf,directed,separator)
