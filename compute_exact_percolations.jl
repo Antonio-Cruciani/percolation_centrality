49a2d3aad6cd12186bbc0505356baa121dfb97a7
Antonio <antonio.cruciani@gssi.it>
2024-12-11 17:21:47 +0100

    new experiments

--- compute_exact_percolations.jl
+++ compute_exact_percolations.jl
@@ -1,7 +1,7 @@
 include("src/PERC.jl")
 
 # Undirected
-
+#=
 #datasets = ["03_ca_astroph.txt","07_large_twitch_edges.txt"]
 datasets = ["00_hiv.txt","00_ego-fb-combined-N.txt"]
 directed = false
@@ -20,10 +20,10 @@ for ds in datasets
     x = parallel_percolation_centrality(g,perc,normalized)
     save_results(ds_name,"exact",x[1],x[2])
 end
-#=
+=#
 # Directed
 #datasets = ["04_web_notredame.txt","06_web_google.txt","08_web_berkstan.txt"]
-datasets = ["09_italian_twitter.txt"]
+datasets = ["05_wiki_talk","13_soc_pokec.txt"]
 
 
 directed = true
@@ -41,4 +41,4 @@ for ds in datasets
     save_results(ds_name,"exact",x[1],x[2])
 end
 
-=#
+
--- generate_random_percolation_states.jl
+++ generate_random_percolation_states.jl
@@ -4,8 +4,8 @@ include("src/PERC.jl")
 
 #datasets = ["00_hiv.txt","00_ego-fb-combined-N.txt","01_musae_facebook_edges.txt","02_email_enron.txt","03_ca_astroph.txt","07_large_twitch_edges.txt","15_cit_hepph.txt","14_p2p_gnutella31.txt","11_soc_epinions.txt","12_soc_slashdot.txt","04_web_notredame.txt","06_web_google.txt","08_web_berkstan.txt"]
 
-datasets = ["09_italian_twitter.txt","10_flickr.txt"]
-
+#datasets = ["09_italian_twitter.txt","10_flickr.txt"]
+datasets = ["05_wiki_talk","13_soc_pokec.txt"]
 
 directed = false
 
