670e70781a6bc750c5263f317d3495e50c97fd3c
Antonio <antonio.cruciani@gssi.it>
2024-12-14 20:00:00 +0100

    new version exact algorithm s-z and experiments for undirected graphs

--- compute_exact_percolations_target.jl
+++ compute_exact_percolations_target.jl
@@ -3,11 +3,10 @@ include("src/PERC.jl")
 # Undirected
 
 
-datasets = ["00_hiv.txt","00_ego-fb-combined-N.txt"]
+datasets = ["00_hiv.txt","00_ego-fb-combined-N.txt","01_musae_facebook_edges","02_email_enron", "03_ca_astroph","07_large_twitch_edges" ,"10_flickr"]
 directed = false
 
 separator = "\t"
-normalized = false
 percolation_path = "percolation_states/"
 graphs_path = "graphs/"
 for ds in datasets
@@ -18,18 +17,20 @@ for ds in datasets
     @info("Computing Ground Truth percolation centrality for "*ds_name)
     flush(stderr)
     
-    x = parallel_percolation_centrality_target(g,perc,normalized)
-    save_results(ds_name,"exact_target",x[1],x[2])
+    x = parallel_percolation_centrality_target(g,perc)
+    save_results(ds_name,"exact_target",x[1],x[3])
+    save_results(ds_name,"exact_target_unnormalized",x[2],x[3])
+
 end
 #=
 # Directed
 #datasets = ["15_cit_hepph.txt","14_p2p_gnutella31.txt","11_soc_epinions.txt","12_soc_slashdot.txt"]
-datasets = ["09_italian_twitter.txt","13_soc_pokec.txt"]
+datasets = [ "15_cit_hepph" ,"14_p2p_gnutella31","11_soc_epinions" ,"12_soc_slashdot","04_web_notredame" ,"06_web_google","08_web_berkstan"  ,"09_italian_twitter","05_wiki_talk","13_soc_pokec.txt"]                 
+
 
 directed = true
 
 separator = "\t"
-normalized = false
 for ds in datasets
     gf = graphs_path*ds
     g = load_graph(gf,directed,separator)
@@ -37,9 +38,11 @@ for ds in datasets
     ds_name = string(split(ds,".txt")[1])
     @info("Computing Ground Truth percolation centrality for "*ds_name)
     flush(stderr)
-    x = parallel_percolation_centrality_target(g,perc,normalized)
-    save_results(ds_name,"exact_target",x[1],x[2])
+    x = parallel_percolation_centrality_target(g,perc)
+    save_results(ds_name,"exact_target",x[1],x[3])
+    save_results(ds_name,"exact_target_unnormalized",x[2],x[3])
 end
+
 
 
 =#
--- src/parallel/parallel_percolation_centrality.jl
+++ src/parallel/parallel_percolation_centrality.jl
@@ -108,7 +108,7 @@ function _per_cent_accumulate!(u::Int64,g,percolation_states::Array{Float64},sum
             end
         end
         
-        #println("Processed "*string(u)*"/"*string(n)*" in time "*string(time()-start_time))
+        #println("Processed "*string(u)*"/"*sx(w)]tring(n)*" in time "*string(time()-start_time))
     
     
     return nothing
@@ -117,7 +117,7 @@ end
 
 
 
-function parallel_percolation_centrality_target(g,percolation_states::Array{Float64},normalized::Bool = true)::Tuple{Array{Float64},Float64}
+function parallel_percolation_centrality_target(g,percolation_states::Array{Float64})::Tuple{Array{Float64},Array{Float64},Float64}
     @assert nv(g) == lastindex(percolation_states) "Vertex set and percolation array must have the same dimensions"
     @info("----------------------------------------| Stats |--------------------------------------------------")
     @info("Analyzing graph")
@@ -129,27 +129,16 @@ function parallel_percolation_centrality_target(g,percolation_states::Array{Floa
     @info("Average Percolated state "*string(mean(percolation_states))*" std "*string(std(percolation_states)))
     @info("Using "*string(nthreads())* " Threads")
     @info("---------------------------------------------------------------------------------------------------")
-    @info("Computing percolation centrality")
+    @info("Computing percolation centrality considering source and targets")
     flush(stderr)
     start_time::Float64 = time()
     n::Int64 = nv(g)
     #percent_status::Array{Tuple{Float64,Int64,String}} = [(0.25,trunc(Int64,n*0.25),"Analyzed 25% of the graph"),(0.50,trunc(Int64,n*0.50),"Analyzed 50% of the graph"),(0.75,trunc(Int64,n*0.75),"Analyzed 75% of the graph"),(0.9,trunc(Int64,n*0.9),"Analyzed 90% of the graph")]
     #percent_index::Int16 = 1
-    all_couples::Array{Tuple{Int64,Int64}} = Array{Tuple{Int64,Int64}}([])
-    for i in 1:n
-        for j in 1:n
-            if i!=j
-                push!(all_couples,(i,j))
-            end
-        end
-    end
-    N = lastindex(all_couples)
-    @info("Overall number of couples "*string(N))
-    flush(stderr)
-    vs_active = [i for i in 1:N]
-    d, r = divrem(N, nthreads())
+    vs_active = [i for i in 1:n]
+    d, r = divrem(n, nthreads())
     ntasks = d == 0 ? r : nthreads()
-    task_size = cld(N, ntasks)
+    task_size = cld(n, ntasks)
     percolation::Array{Array{Float64}} = [zeros(Float64,n) for _ in 1:ntasks]
     final_percolation::Array{Float64} = zeros(Float64,n)
     #tmp_perc_states::Array{Float64} = copy(percolation_states)
@@ -157,14 +146,12 @@ function parallel_percolation_centrality_target(g,percolation_states::Array{Floa
     tmp_perc_states::Dict{Int64,Float64} = Dict(v => percolation_states[v] for v in 1:n)
     sorted_dict = OrderedDict(sort(collect(tmp_perc_states), by = kv -> kv[2]))
     percolation_data::Tuple{Float64,Dict{Int64,Float64}} = percolation_differences(sorted_dict,n)
-    verbose_step::Int64 = trunc(Int64,floor(N*0.25))
+    verbose_step::Int64 = trunc(Int64,floor(n*0.25))
     processed_so_far::Int64 = 0
-    @sync for (t, task_range) in enumerate(Iterators.partition(1:N, task_size))
-        Threads.@spawn for i in @view(vs_active[task_range])
-            s = all_couples[i][1]
-            z = all_couples[i][2]
-            if length(outneighbors(g,s)) > 0 && length(inneighbors(g,z)) > 0 && ramp(percolation_states[s],percolation_states[z]) > 0
-                _parallel_sz_bfs_exact!(g,s,z,n,percolation_states,percolation_data,percolation[t])
+    @sync for (t, task_range) in enumerate(Iterators.partition(1:n, task_size))
+        Threads.@spawn for s in @view(vs_active[task_range])
+            if length(outneighbors(g,s)) > 0
+                _parallel_sz_bfs_exact!(g,s,percolation_states,percolation[t])
             end
 
             if (Sys.free_memory() / Sys.total_memory() < 0.1)
@@ -175,99 +162,64 @@ function parallel_percolation_centrality_target(g,percolation_states::Array{Floa
             if (verbose_step > 0 && processed_so_far % verbose_step == 0)
                 finish_partial::String = string(round(time() - start_time; digits=4))
                 time_to_finish::String = string(round((n*(time() - start_time) / processed_so_far )-(time() - start_time) ; digits=4))
-                @info("Percolation Centrality. Processed " * string(processed_so_far) * "/" * string(N) * " nodes in " * finish_partial * " seconds | Est. remaining time : "*time_to_finish)
+                @info("Percolation Centrality. Processed " * string(processed_so_far) * "/" * string(n) * " nodes in " * finish_partial * " seconds | Est. remaining time : "*time_to_finish)
                 flush(stderr)
             end
         end
     end
     final_percolation = reduce(+,percolation)
-    if normalized
-        #final_percolation = final_percolation .* [1/(n*(n-1))]
-        final_percolation = final_percolation .* [1/(n-2)]
+    # Normalizing using percolation states 
+    unnormalized_scores::Array{Float64} = copy(final_percolation)
+    for v in 1:n
+        final_percolation[v] = final_percolation[v] *(1/percolation_data[2][v])
+
     end
     finish_time::Float64 = time()-start_time
     @info("Percolation centrality s to z computed in "*string(finish_time))
     flush(stderr)
-    return final_percolation,finish_time
+    return final_percolation,unnormalized_scores,finish_time
 end
 
 
-function _parallel_sz_bfs_exact!(g,s::Int64,z::Int64,n::Int64,percolation_states::Array{Float64},percolation_data::Tuple{Float64,Dict{Int64,Float64}},B_1::Array{Float64})
-    q::Queue{Int64} = Queue{Int64}()
+function _parallel_sz_bfs_exact!(g,u::Int64,percolation_states::Array{Float64},percolation::Array{Float64})
+    n::Int64 = nv(g)
+    w::Int64 = -1
+    delta::Array{Float64} = zeros(Float64,n)
+    dist::Array{Int64} = zeros(Int64,n)
     ball::Array{Int16} = zeros(Int16,n)
     n_paths::Array{UInt128} = zeros(UInt128,n)
-    dist::Array{Int64} = zeros(Int64,n)
-    pred::Array{Array{Int64}} = [Array{Int64}([]) for _ in 1:n]
-
+    pred::Array{Array{Int64}} = Array{Array{Int64}}([[] for _ in 1:n])
+    q::Queue{Int64} = Queue{Int64}()
+    s::Stack{Int64} = Stack{Int64}()
     
-    w::Int64 = 0
-    d_z_min::Float64 = Inf
-    #q_backtrack::Stack{Int64} = Stack{Int64}()
-    
-    enqueue!(q,s)
-    dist[s] = 0
-    n_paths[s] = 1
-    ball[s] = 1
-    while length(q) != 0
+    enqueue!(q,u)
+    dist[u] = 0
+    ball[u] = 1
+    n_paths[u] = 1
+    while length(q)!= 0
         w = dequeue!(q)
-        if dist[w] < d_z_min
-            for v in outneighbors(g,w)
-                if (ball[v] == 0)
-                    dist[v] = dist[w] +1
-                    ball[v] = 1
-                    n_paths[v] = n_paths[w]
-                    if (v == z)
-                        if dist[v] < d_z_min
-                            d_z_min = dist[v]
-                        end
-                        #=
-                        if length(q_backtrack) == 0
-                            enqueue!(q_backtrack,z)
-                        end
-                        =#
-                    end
-                    push!(pred[v],w)
+        for v in outneighbors(g,w)
+            if ball[v] == 0
+                dist[v] = dist[w]+1
+                ball[v] = 1
                     enqueue!(q,v)
-                    #push!(q_backtrack,v)
-                elseif (dist[v] == dist[w] + 1)
+                    push!(s,v)
+                end
+                if (ball[v] == 1 && dist[v] == dist[w]+1)
                     n_paths[v] += n_paths[w]
                     push!(pred[v],w)
                 end
-
             end
         end
-    end
-    #backtrack
-
-   if ball[z] == 1
-        q_backtrack::Queue{Int64} = Queue{Int64}()
-        queued::Array{Int16} = zeros(Int16,n)
-        #number_of_sps_to_target::Dict{Int64,Int128} = Dict{Int64,Int128}()
-        number_of_sps_to_target::Array{UInt128} = zeros(UInt128,n)
-        number_of_paths_through_curr::UInt128 = 0
-        for p in pred[z]
-            enqueue!(q_backtrack,p)
-            number_of_sps_to_target[p] = 1
-            queued[p] = 1
-        end
-        w = dequeue!(q_backtrack)
-        while w != s
-            number_of_paths_through_curr = n_paths[w] * number_of_sps_to_target[w]
-            summand = (number_of_paths_through_curr/n_paths[z]) *(ramp(percolation_states[s],percolation_states[z])/percolation_data[2][w])  
-            
-            B_1[w] += summand           
-
+        while length(s)!= 0
+            w = pop!(s)
             for p in pred[w]
-                if queued[p] == 0
-                    enqueue!(q_backtrack,p)
-                    queued[p] = 1
-                end
-                number_of_sps_to_target[p] += number_of_sps_to_target[w]
+                delta[p] += n_paths[p]/n_paths[w] * (ramp(percolation_states[u],percolation_states[w])+delta[w])
             end
-            w  = dequeue!(q_backtrack)
+            if w!= u
+                percolation[w] += delta[w]
+            end
         end
-    end
-
 
    return nothing
 end
