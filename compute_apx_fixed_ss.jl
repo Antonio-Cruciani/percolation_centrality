d24f2499c23a9acf6361a6208092238fc911254a
Antonio <antonio.cruciani@gssi.it>
2025-01-26 21:46:27 +0100

    new version

--- compute_apx_fixed_ss.jl
+++ compute_apx_fixed_ss.jl
@@ -15,6 +15,8 @@ output_path = "/home/pasquini/My_nas/data_percolation/"
 graphs_path = "graphs/"
 percolation_path = "percolation_states/"
 output_path = ""
+
+#=
 datasets = ["01_musae_facebook_edges.txt","02_email_enron.txt","03_ca_astroph.txt","07_large_twitch_edges.txt","10_flickr.txt"]
 #datasets = ["com-youtube.ungraph.txt","com-lj.ungraph.txt","com-orkut.ungraph.txt"]
 
@@ -38,9 +40,9 @@ for ds in datasets
     end
 end
 
+=#
 
 
-#=
 # Directed
 datasets = ["15_cit_hepph.txt","14_p2p_gnutella31.txt","11_soc_epinions.txt","12_soc_slashdot.txt","04_web_notredame.txt","06_web_google.txt","08_web_berkstan.txt","05_wiki_talk.txt","09_italian_twitter.txt","13_soc_pokec.txt"]
 
@@ -64,4 +66,4 @@ for ds in datasets
         i+=1
     end
 end
-=#
+
