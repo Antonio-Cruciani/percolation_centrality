14fbbd1e3dd60e7374593cd11eb3b0ae56ddbe3a
Antonio <antonio.cruciani@gssi.it>
2025-03-31 11:14:03 +0300

    new experiments for uniform sampling vs non-uniform (exact values)

---
+++ uniform_vs_non_uniform_sampling.jl
@@ -0,0 +1,52 @@
+include("src/PERC.jl")
+
+
+datasets = ["01_musae_facebook_edges.txt","02_email_enron.txt", "03_ca_astroph.txt"]
+
+
+
+epsilon_list = [0.1,0.07,0.05,0.01,0.005,0.001,0.0005]
+ss_save = [1,2,3,4,5,6,7]
+delta = 0.05
+
+run = 5
+
+graphs_path = "graphs/"
+percolation_path = "percolation_states/"
+output_path = ""
+component_size = 50
+
+directed = false
+separator = "\t"
+
+for ds in datasets
+    gf = graphs_path*ds
+    g = load_graph(gf,directed,separator)
+    h,x = attach_gadget(g,component_size)
+    ds_name = string(split(ds,".txt")[1])*"_"*string(component_size)
+    save_percolation_array(ds_name,x)
+    save_graph(h,ds_name,separator)
+    @info("Computing Ground Truth percolation centrality for "*ds_name)
+    flush(stderr)
+    y = parallel_percolation_centrality_new_target(h,x)
+    save_results_new(ds_name,"exact_target",y[1],y[6],y[5])
+    #save_results(ds_name,"exact_target_unnormalized",x[2],x[3])
+end
+
+datasets = ["15_cit_hepph.txt" ,"14_p2p_gnutella31.txt","11_soc_epinions.txt"]
+
+directed = true
+
+for ds in datasets
+    gf = graphs_path*ds
+    g = load_graph(gf,directed,separator)
+    h,x = attach_gadget(g,component_size)
+    ds_name = string(split(ds,".txt")[1])*"_"*string(component_size)
+    save_percolation_array(ds_name,x)
+    save_graph(h,ds_name,separator)
+    @info("Computing Ground Truth percolation centrality for "*ds_name)
+    flush(stderr)
+    y = parallel_percolation_centrality_new_target(h,x)
+    save_results_new(ds_name,"exact_target",y[1],y[6],y[5])
+    #save_results(ds_name,"exact_target_unnormalized",x[2],x[3])
+end
