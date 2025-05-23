7a67b1f4ad09a4813ab31d1697dbf10c9aec3ce9
Antonio <antonio.cruciani@gssi.it>
2025-05-23 14:27:31 +0300

    new version

--- compute_diameter.jl
+++ compute_diameter.jl
@@ -5,12 +5,12 @@ include("src/PERC.jl")
 graph_path = "graphs/"
 
 # Undirected
-datasets = ["01_musae_facebook_edges.txt","02_email_enron.txt","03_ca_astroph.txt","24_uselections.txt","23_twitter_pol.txt","22_obamacare.txt","21_brexit.txt","20_abortion.txt"]
+datasets = ["01_musae_facebook_edges.txt","02_email_enron.txt","03_ca_astroph.txt"]
 
 
 graphs_path = "graphs/"
 percolation_path = "percolation_states/"
-separator = " "
+separator = "\t"
 directed = false
 for ds in datasets
     gf = graphs_path*ds
@@ -28,7 +28,7 @@ end
 datasets = ["15_cit_hepph.txt" ,"14_p2p_gnutella31.txt","11_soc_epinions.txt","12_soc_slashdot.txt","04_web_notredame.txt","06_web_google.txt"]
 graphs_path = "graphs/"
 percolation_path = "percolation_states/"
-separator = " "
+separator = "\t"
 directed = true
 for ds in datasets
     gf = graphs_path*ds
@@ -38,4 +38,23 @@ for ds in datasets
     x,y,t = parallel_random_bfs_rho(g,0)
     save_diameter(ds_name,x,y,t)
 end
+
+
+# Undirected
+datasets = ["24_uselections.txt","23_twitter_pol.txt","22_obamacare.txt","21_brexit.txt","20_abortion.txt"]
+
+
+graphs_path = "graphs/"
+percolation_path = "percolation_states/"
+separator = " "
+directed = false
+for ds in datasets
+    gf = graphs_path*ds
+    g = load_graph(gf,directed,separator)
+    ds_name = string(split(ds,".txt")[1])
+    @info("Computing Ground Truth Diameter for "*ds_name)
+    x,y,t = parallel_random_bfs_rho(g,0)
+    save_diameter(ds_name,x,y,t)
+end
+
 
